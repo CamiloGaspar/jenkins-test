5382c5c71c734bd60ebfc68163ee7138f13d1680
Juan Camilo Gaspar Arias <camilo.gaspar@payulatam.com>
2021-09-26 19:20:37 -0500

    first commit

---
+++ src/main/java/co/com/jcga/appgate/calculator/AppgateCalculatorApplication.java
@@ -0,0 +1,14 @@
+package co.com.jcga.appgate.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgateCalculatorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppgateCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/jcga/appgate/calculator/AppgateCalculatorApplicationTests.java
@@ -0,0 +1,11 @@
+package co.com.jcga.appgate.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppgateCalculatorApplicationTests {
+
+
+
+}
